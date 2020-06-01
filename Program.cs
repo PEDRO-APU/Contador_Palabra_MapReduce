32d7da85e0d8ac5c98a46c76a1da8a2018bbc422
PEDRO-APU <62312971+PEDRO-APU@users.noreply.github.com>
2020-06-01 20:11:25 -0300

    subo

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ContadorPalabras
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("ingrese parrafo: ");
+            string parrafo = Console.ReadLine();
+
+
+            Dictionary<string, int> lista = new Dictionary<string, int>();
+            string[] solopalabras = parrafo.Split();
+
+            Parallel.ForEach(solopalabras, //objeto iterable almacen de datos
+                () => { return new Dictionary<String, int>(); }, //inicializamos el almacen local TLS
+                (palabra, loopControl, localD) =>//cueprpo del algoritmo concurrente.
+                {
+                    string pal = Convert.ToString(palabra);
+                    if (!localD.ContainsKey(palabra))
+                    {
+                        localD.Add(palabra, 1);
+                    }
+                    else
+                    {
+                        localD[palabra]++;
+                    }
+                    return localD;
+                },
+               (localReduce) =>// finalizador (Reduce)
+               {
+                   lock(lista)
+                   {
+                       foreach (String palabra in localReduce.Keys)
+                       {
+
+
+                           if (!lista.ContainsKey(palabra))
+                           {
+                               lista.Add(palabra, 1);
+                           }
+                           else
+                           {
+                               lista[palabra]++;
+                           }
+                       
+                       }
+
+
+                   }
+               }
+            );
+            foreach(KeyValuePair<string, int> par in lista)
+            {
+                Console.WriteLine("Letra: {0} valor: {1}", par.Key, par.Value);
+            }
+                
+           
+            
+            Console.ReadKey();
+        }
+    }
+}
+
